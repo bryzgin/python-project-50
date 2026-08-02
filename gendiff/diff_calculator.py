0c4d0e7f46c4f035798da58b27b364f8f1f12b77
k.bryzgin <k.bryzgin@yandex.ru>
2026-08-02 13:49:30 +0700

    feat: generate_diff mod

--- gendiff/diff_calculator.py
+++ gendiff/diff_calculator.py
@@ -2,6 +2,12 @@ import json
 import os
 
 
+def to_str(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return str(value)
+
+
 def generate_diff(file_path1, file_path2):
     abs_path1 = os.path.abspath(file_path1)
     abs_path2 = os.path.abspath(file_path2)
@@ -12,7 +18,19 @@ def generate_diff(file_path1, file_path2):
     with open(abs_path2, "r") as f2:
         data2 = json.load(f2)
     
-    print("Файл 1 распарсен:", data1)
-    print("Файл 2 распарсен:", data2)
+    all_keys = sorted(list(data1.keys() | data2.keys()))
     
-    return ""
+    lines = ["{"]
+    
+    for key in all_keys:
+        if key in data1 and key in data2:
+            lines.append(f" - {key}: {to_str(data1[key])}")        
+        elif key not in data1 and key in data2:
+            lines.append(f" - {key}: {to_str(data1[key])}")
+            lines.append(f" + {key}: {to_str(data2[key])}")        
+        else:
+            lines.append(f"   {key}: {to_str(data1[key])}")
+    
+    lines.append("}")
+    
+    return "\n".join(lines)

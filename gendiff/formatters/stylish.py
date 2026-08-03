5a79e0191edf3a4fcca6bf8995cc44a65ccfa0bd
k.bryzgin <k.bryzgin@yandex.ru>
2026-08-03 19:45:55 +0700

    feat: add render_stylish function

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -18,3 +18,29 @@ def stringify(value, depth):
     return "\n".join(lines)
 
 
+def render_stylish(tree, depth=1):
+    indent = "    " * depth    
+    sign_indent = indent[:-2]
+    
+    lines = ["{"]
+    
+    for nod in tree:
+        node_type = node["type"]
+        key = node["key"]
+        
+        if node_type == "nested":
+            lines.append(f"{sign_indent}  {key}: {render_stylish(node["children"], depth + 1)}")
+        elif node_type == "added":
+            lines.append(f"{sign_indent}+ {key}: {stringify(node["value"], depth + 1)}")
+        elif node_type == "deleted":
+            lines.append(f"{sign_indent}- {key}: {stringify(node["children"], depth + 1)}")
+        elif node_type == "unchanged":
+            lines.append(f"{sign_indent}  {key}: {stringify(node["children"], depth + 1)}")
+        elif node_type == "changed":
+            lines.append(f"{sign_indent}- {key}: {stringify(node["old_value"], depth + 1)}")
+            lines.append(f"{sign_indent}+ {key}: {stringify(node["new_value"], depth + 1)}")
+    
+    closing_indent = "    " * (depth - 1)
+    lines.append(f"{closing_indent}}")
+    
+    return "\n".join(lines)
--- gendiff/tree_builder.py
+++ gendiff/tree_builder.py
@@ -11,30 +11,26 @@ def build_diff_tree(data1, data2):
                 "key": key,
                 "type": "nested",
                 "children": build_diff_tree(val1, val2)
-            })
-        
+            })        
         elif key in data1 and key not in data2:
             tree.append({
                 "key": key,
                 "type": "deleted",
                 "value": val1
-            })
-        
+            })        
         elif key not in data1 and key in data2:
             tree.append({
                 "key": key,
                 "type": "added",
                 "value": val2
-            })
-        
+            })        
         elif val1 != val2:
             tree.append({
                 "key": key,
                 "type": "changed",
                 "old_value": val1,
                 "new_value": val2
-            })
-        
+            })        
         else:
             tree.append({
                 "key": key,
